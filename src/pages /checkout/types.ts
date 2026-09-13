28dfa6e799b6fae7e15074b668ac53bf2fcc09a7
Sunil <om.gamal10@gmail.com>
2026-09-13 12:36:33 +0545

    for changes made in order

--- src/pages /checkout/types.ts
+++ src/pages /checkout/types.ts
@@ -1,45 +1,44 @@
 import { Status } from "../../globals/types/type";
 import { IOrderDetail } from "../my-orders-details/types";
 
-interface IProduct{
-    productId : string, 
-    productQty : number, 
-    orderStatus? : string, 
-    totalAmount?:number, 
-    Payment? : {
-        paymentMethod : PaymentMethod, 
-        
-    }
+interface IProduct {
+  productId: string;
+  productQty: number;
+  orderStatus?: string;
+  totalAmount?: number;
+  Payment?: {
+    paymentMethod: PaymentMethod;
+  };
 }
-export interface IOrderItems extends IProduct{
-    id : string, 
-    orderId : string
-}
-
-export interface IOrder{
-    status : Status, 
-    items : IOrderItems[], 
-    khaltiUrl : string | null , 
-    orderDetails : IOrderDetail[]
-
+export interface IOrderItems extends IProduct {
+  id: string;
+  orderId: string;
 }
 
-export enum PaymentMethod{
-    Esewa = "esewa", 
-    Khalti = "khalti", 
-    Cod = "cod"
+export interface IOrder {
+  status: Status;
+  items: IOrderItems[];
+  khaltiUrl: string | null;
+  orderDetails: IOrderDetail[];
+  redirect?: boolean;
 }
 
-export interface IData{
-    firstName : string, 
-    lastName : string, 
-    phoneNumber : string, 
-    email : string, 
-    city : string, 
-    zipCode : string, 
-    state : string, 
-    addressLine : string, 
-    totalAmount : number, 
-    paymentMethod : PaymentMethod, 
-    products : IProduct[]
+export enum PaymentMethod {
+  Esewa = "esewa",
+  Khalti = "khalti",
+  Cod = "cod",
+}
+
+export interface IData {
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  email: string;
+  city: string;
+  zipCode: string;
+  state: string;
+  addressLine: string;
+  totalAmount: number;
+  paymentMethod: PaymentMethod;
+  products: IProduct[];
 }
--- src/pages /product/Product.tsx
+++ src/pages /product/Product.tsx
@@ -1,44 +1,42 @@
-import { useEffect } from "react"
-import Navbar from "../../globals/components/Navbar"
-import Card from "./components/Card"
-import { useAppDispatch, useAppSelector } from "../../store/hooks"
-import { fetchProducts } from "../../store/productSlice"
+import { useEffect } from "react";
+import Navbar from "../../globals/components/Navbar";
+import Card from "./components/Card";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { fetchProducts } from "../../store/productSlice";
 
+function Product() {
+  const dispatch = useAppDispatch();
+  const { products, status } = useAppSelector((store) => store.products);
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, []);
 
-function Product(){
-    const dispatch = useAppDispatch()
-    const {products,status} = useAppSelector((store)=>store.products)
-    useEffect(()=>{
-        dispatch(fetchProducts())
-    },[])
+  return (
+    <>
+      <Navbar />
+      <div>
+        <section
+          id="Projects"
+          className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5"
+        >
+          {products.length > 0 &&
+            products.map((product) => {
+              return <Card product={product} />;
+            })}
+          {products.length == 0 && <div> no product found</div>}
+        </section>
 
-    return ( 
-        <>
-        <Navbar/>
- <div>
-  <section id="Projects" className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
-    {products.length > 0 && products.map((product)=>{
-        return (
-
-            <Card product={product} />
-        )
-    })}
-
-
-  </section>
-
-  {/* Support Me 🙏🥰 */}
-  <script>
-    kofiWidgetOverlay.draw('mohamedghulam', {'{'}
-    'type': 'floating-chat',
-    'floating-chat.donateButton.text': 'Support me',
-    'floating-chat.donateButton.background-color': '#323842',
-    'floating-chat.donateButton.text-color': '#fff'
-    {'}'});
-  </script></div>
-
-        </>
-    )
+        {/* Support Me 🙏🥰 */}
+        <script>
+          kofiWidgetOverlay.draw('mohamedghulam', {"{"}
+          'type': 'floating-chat', 'floating-chat.donateButton.text': 'Support
+          me', 'floating-chat.donateButton.background-color': '#323842',
+          'floating-chat.donateButton.text-color': '#fff'
+          {"}"});
+        </script>
+      </div>
+    </>
+  );
 }
 
-export default Product
+export default Product;
