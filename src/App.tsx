6d73e514875f4960ab840964a0f249bfc643b089
Sunil <om.gamal10@gmail.com>
2026-09-13 12:45:13 +0545

    login fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,59 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages /user/Register";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import Home from "./pages /home/Home";
+import Login from "./pages /user/Login";
+import Product from "./pages /product/Product";
+import SingleProduct from "./pages /single-product/SingleProduct";
+import MyCart from "./pages /cart/my-cart";
+import Checkout from "./pages /checkout/Checkout";
+import MyOrder from "./pages /my-orders/MyOrder";
+import MyOrderDetail from "./pages /my-orders-details/MyOrderDetail";
 
-import './App.css'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import Register from './pages /user/Register'
-import { Provider } from 'react-redux'
-import store from './store/store'
-import Home from './pages /home/Home'
-import Login from './pages /user/Login'
-import Product from './pages /product/Product'
-import SingleProduct from './pages /single-product/SingleProduct'
-import MyCart from './pages /cart/my-cart'
-import Checkout from './pages /checkout/Checkout'
-import MyOrder from './pages /my-orders/MyOrder'
-import MyOrderDetail from './pages /my-orders-details/MyOrderDetail'
-import AdminIndex from './pages /admin/AdminLayout'
-import Categories from './pages /admin/categories/Categories'
-import AdminLayout from './pages /admin/AdminLayout'
-import AdminStats from './pages /admin/stats/AdminStats'
-import User from './pages /admin/users/Users'
-import AdminProduct from './pages /admin/products/Product'
-import ProductDescription from './pages /admin/productDescription/ProductDescription'
-import AdminOrder from './pages /admin/orders/AdminOrder'
-import AdminOrderDetail from './pages /admin/order-details/AdminOrderDetails'
-import { io } from 'socket.io-client'
-export const socket = io("http://localhost:3000",{
-  auth : {
-    token  : localStorage.getItem("tokenHoYo")
-  }
-})
+import Categories from "./pages /admin/categories/Categories";
 
+import AdminStats from "./pages /admin/stats/AdminStats";
+import User from "./pages /admin/users/Users";
+import AdminProduct from "./pages /admin/products/Product";
+import ProductDescription from "./pages /admin/productDescription/ProductDescription";
+import AdminOrder from "./pages /admin/orders/AdminOrder";
+import AdminOrderDetail from "./pages /admin/order-details/AdminOrderDetails";
+import { io } from "socket.io-client";
+import AdminLogin from "./pages /admin/login";
+export const socket = io("http://localhost:3000", {
+  auth: {
+    token: localStorage.getItem("tokenHoYo"),
+  },
+});
 
 function App() {
-
   return (
-  <Provider store={store}>
-     <BrowserRouter>
-    <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='/register' element={<Register />}/>
-    <Route path='/login' element={<Login />}/>
-    <Route path='/products' element={<Product />}/>
-    <Route path='/products/:id' element={<SingleProduct />}/>
-    <Route path="/my-cart" element={<MyCart />} />
-    <Route path="/my-checkout" element={<Checkout />} />
-    <Route path="/my-orders" element={<MyOrder />} />
-    <Route path="/my-orders/:id" element={<MyOrderDetail />} />
-    <Route path='/admin' element={<AdminStats />} />
-    <Route path='/admin/categories' element={<Categories />} />
-    <Route path='/admin/users' element={<User />} />
-    <Route path='/admin/products' element={<AdminProduct />} />
-    <Route path='/admin/orders' element={<AdminOrder />} />
-    <Route path='/admin/products/:id' element={<ProductDescription />} />
-    <Route path='/admin/orders/:id' element={<AdminOrderDetail />} />
-    </Routes>
-    </BrowserRouter>
-  </Provider>
-  )
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/products" element={<Product />} />
+          <Route path="/products/:id" element={<SingleProduct />} />
+          <Route path="/my-cart" element={<MyCart />} />
+          <Route path="/my-checkout" element={<Checkout />} />
+          <Route path="/my-orders" element={<MyOrder />} />
+          <Route path="/my-orders/:id" element={<MyOrderDetail />} />
+          <Route path="/admin/login" element={<AdminLogin />} />
+          <Route path="/admin" element={<AdminStats />} />
+          <Route path="/admin/categories" element={<Categories />} />
+          <Route path="/admin/users" element={<User />} />
+          <Route path="/admin/products" element={<AdminProduct />} />
+          <Route path="/admin/orders" element={<AdminOrder />} />
+          <Route path="/admin/products/:id" element={<ProductDescription />} />
+          <Route path="/admin/orders/:id" element={<AdminOrderDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
--- src/pages /user/Login.tsx
+++ src/pages /user/Login.tsx
@@ -1,77 +1,107 @@
-import { ChangeEvent, FormEvent, useEffect, useState } from "react"
-import { useAppDispatch, useAppSelector } from "../../store/hooks"
-import { loginUser, registerUser } from "../../store/authSlice"
-import { Link } from "react-router-dom"
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { loginUser, registerUser } from "../../store/authSlice";
+import { Link, useNavigate } from "react-router-dom";
+import { Status } from "../../globals/types/type";
 
+function Login() {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const { status, user } = useAppSelector((store) => store.auth);
 
-function Login(){
-    const dispatch = useAppDispatch()
-    const {status,user} = useAppSelector((store)=>store.auth)
-
-    const [data,setData] = useState({
-        password : "", 
-        email : ""
-    })
-    const handleChange = (e:ChangeEvent<HTMLInputElement>)=>{
-        const {name,value} = e.target 
-        setData({
-            ...data, 
-            [name] : value
-        })
+  useEffect(() => {
+    if (status === Status.SUCCESS) {
+      // Handle successful login
+      navigate("../");
     }
+  }, [status, navigate]);
 
-    const handleSubmit = (e:FormEvent<HTMLFormElement>)=>{
-        e.preventDefault()
-        dispatch(loginUser(data))
-    }
+  const [data, setData] = useState({
+    password: "",
+    email: "",
+  });
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
 
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(loginUser(data));
+  };
 
-    return ( 
-        <div className="bg-gray-100 flex h-screen items-center justify-center px-4 sm:px-6 lg:px-8">
-    <div className="w-full max-w-md space-y-8">
+  return (
+    <div className="bg-gray-100 flex h-screen items-center justify-center px-4 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md space-y-8">
         <div className="bg-white shadow-md rounded-md p-6">
+          <img
+            className="mx-auto h-12 w-auto"
+            src="https://www.svgrepo.com/show/499664/user-happy.svg"
+            alt=""
+          />
 
-            <img className="mx-auto h-12 w-auto" src="https://www.svgrepo.com/show/499664/user-happy.svg" alt="" />
+          <h2 className="my-3 text-center text-3xl font-bold tracking-tight text-gray-900">
+            Sign In , {user?.username}
+          </h2>
 
-            <h2 className="my-3 text-center text-3xl font-bold tracking-tight text-gray-900">
-                Sign In , {user?.username}
-            </h2>
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email
+              </label>
+              <div className="mt-1">
+                <input
+                  name="email"
+                  type="email-address"
+                  autoComplete="email-address"
+                  required
+                  className="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
 
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <div className="mt-1">
+                <input
+                  name="password"
+                  type="password"
+                  autoComplete="password"
+                  required
+                  className="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm"
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
 
-            <form className="space-y-6" onSubmit={handleSubmit}>
-
-
-                <div>
-                    <label htmlFor="password" className="block text-sm font-medium text-gray-700">Email</label>
-                    <div className="mt-1">
-                        <input name="email" type="email-address" autoComplete="email-address" required
-                            className="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm" onChange={handleChange} />
-                    </div>
-                </div>
-
-                <div>
-                    <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-                    <div className="mt-1">
-                        <input name="password" type="password" autoComplete="password" required
-                            className="px-2 py-3 mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-sky-500 sm:text-sm" onChange={handleChange} />
-                    </div>
-                </div>
-
-         
-
-                <div>
-                    <button type="submit"
-                        className="flex w-full justify-center rounded-md border border-transparent bg-sky-400 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2">Login
-                        Account
-                        </button>
-                </div>
-                <p className="text-blue-500">Wanna Register? <Link to='/register'> Go to Register</Link></p>
-
-            </form>
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md border border-transparent bg-sky-400 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-opacity-75 focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2"
+              >
+                Login Account
+              </button>
+            </div>
+            <p className="text-blue-500">
+              Wanna Register? <Link to="/register"> Go to Register</Link>
+            </p>
+          </form>
         </div>
+      </div>
     </div>
-</div>
-    )
+  );
 }
 
-export default Login
+export default Login;
